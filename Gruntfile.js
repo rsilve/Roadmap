8cbc17b28c6870d8d13eee078a19101d7c0d4764
Robert Silve <robert@silve.net>
2014-07-05 13:27:31 +0200

    Prepare grunt for test

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,9 +8,15 @@ module.exports = function(grunt) {
                     keepalive: true
                 }
             }
+        },
+        // Unit tests.
+        nodeunit: {
+            tests: ['test/*_test.js']
         }
     });
+
     grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-nodeunit');
     grunt.registerTask('default', ['connect']);
 };
 
