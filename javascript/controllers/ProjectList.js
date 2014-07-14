9065b17aef77516106524b1c501dae50d4945335
Robert Silve <robert@silve.net>
2014-07-14 21:27:04 +0200

    add dependencies

--- javascript/controllers/ProjectList.js
+++ javascript/controllers/ProjectList.js
@@ -2,9 +2,10 @@ define([], function () {
 
     return function ($scope, ProjectStore) {
 
+        // Get the data
         $scope.hello = ProjectStore.hello();
 
-
+        // update the data
         $scope.$on(ProjectStore.id, function() {
             $scope.hello = ProjectStore.hello();
         })
