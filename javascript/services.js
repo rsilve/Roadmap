5fdf550213b04f9bbf28b2cb5ad6d85da159279c
Robert Silve <robert@silve.net>
2014-07-17 06:35:57 +0200

    update npm deps

--- javascript/services.js
+++ javascript/services.js
@@ -1,11 +1,13 @@
 define([
     'angular',
     'services/dispatcher',
+    'services/Google'
     'services/ProjectStore'
-], function (angular, dispatcher, ProjectStore) {
+], function (angular, dispatcher, Google, ProjectStore) {
 
     /* services hub */
     return angular.module('Roadmap.services', [])
         .factory("dispatcher", dispatcher)
+        .factory("google", Google)
         .factory("ProjectStore", ['$rootScope', '$timeout', ProjectStore]);
 });
---
+++ javascript/services/Google.js
@@ -0,0 +1,109 @@
+define([
+    "when", "rest", "rest/interceptor/mime"
+], function (when, rest, mime) {
+
+    return function () {
+        // constants for google api
+        var clientId = '914287465512-b14fug3f6kgg1a1t1bm6srvq0d6q5l63.apps.googleusercontent.com';
+        var apiKey = 'AIzaSyCNen5JjpKBaXRgg0oUeD1HpTnfORZY9pw';
+        var scopes = "https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/calendar";
+        var google_api = "https://www.googleapis.com/calendar/v3";
+
+        // auth object is deferred
+        // use it before each request to be sure that
+        // auth process is completed
+        var auth = when.promise(function(resolve, reject, notify) {
+            // start the authorization process
+            gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: true}, function (result) {
+                if (result && !result.error) {
+                    resolve(gapi.auth.getToken());
+                } else {
+                    //actions.authFailed();
+                    reject("Not authorized")
+                }
+            });
+        });
+
+        // helper to create  request
+        function request(c) {
+            c = c || {};
+            c.method = c.method || "GET";
+            c.path = google_api +  c.path;
+            c.entity = c.data;
+
+            return auth.then(function (auth) {
+                c.headers = {
+                    // this header is required by google api
+                    "Authorization": auth.token_type + " " + auth.access_token,
+                    "Content-Type" :  "application/json"
+                };
+                return rest.wrap(mime)(c).then(function(r) {
+                    return r.entity
+                })
+            });
+        }
+
+        // request for getting events list
+        var events = function(calendar) {
+            return request({method: 'GET', path: "/calendars/"+calendar+"/events"});
+        };
+
+        // request for creating a new event
+        // data is a JSON string
+        var createEvent = function(calendar, data) {
+            return request({
+                method: 'POST',
+                path: "/calendars/"+calendar+"/events",
+                data : data
+            });
+        };
+
+        // request for delete an event
+        var deleteEvent = function(calendar, id) {
+            return request({
+                method: 'DELETE',
+                path: "/calendars/"+calendar+"/events/"+id
+            });
+        };
+
+
+        // request for update an event
+        // data is a JSON string
+        var updateEvent = function(calendar, id, data) {
+            return request({
+                method: 'PUT',
+                path: "/calendars/"+calendar+"/events/"+id,
+                data: data
+            });
+        };
+
+        // request for getting calendarList
+        var calendarList = function() {
+            return request({
+                method: 'GET',
+                path: "/users/me/calendarList"
+            });
+        };
+
+
+        // Expose API
+        return function(calendar) {
+            if (calendar)
+                return {
+                    events: function() { return events(calendar) },
+                    createEvent: function(data) { return createEvent(calendar, data) },
+                    deleteEvent: function(id) { return deleteEvent(calendar, id) },
+                    updateEvent: function(id, data) { return updateEvent(calendar, id, data) }
+                };
+            else
+                return {
+                    events: function() { return when.reject("Calendar ID needed") },
+                    createEvent:  function() { return when.reject("Calendar ID needed") },
+                    deleteEvent:  function() { return when.reject("Calendar ID needed") },
+                    updateEvent:  function() { return when.reject("Calendar ID needed") },
+                    calendarList:  function() { return calendarList() }
+                };
+        };
+
+    };
+});
